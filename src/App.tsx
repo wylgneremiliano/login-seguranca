18e63e9150f1d4513b3a7afe71a7952891785b32
= <=>
2025-06-10 21:10:33 -0300

    feat: primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Logo from './assets/logo.png'
+
+function App() {
+
+
+  return (
+    <div className='main'>
+      <div className='container'>
+        <div className='imagemLogin'>
+          <img src={Logo} alt="" width={550} height={345} />
+        </div>
+        <form></form>
+      </div>
+    </div>
+  )
+}
+
+export default App
